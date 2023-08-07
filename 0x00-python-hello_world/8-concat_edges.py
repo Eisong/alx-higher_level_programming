21e7b8d2ed2484593512c6717605f2c442058934
Edet Isong <eddieisong1@gmail.com>
2023-08-07 12:12:45 -0700

    This Python project 0x00. Python - Hello, World by Edet Isong

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[0:67] + str[68:130] + str[:6]
+str = str[0:67] + str[68:130]
 print(str)
