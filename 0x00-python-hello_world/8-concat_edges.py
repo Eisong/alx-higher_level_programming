6f02c7e14674c35a255b96d33db7a9cfd884fb10
Edet Isong <eddieisong1@gmail.com>
2023-08-08 08:29:35 -0700

    My Python project, 0x00. Python - Hello, World by Edet Isong

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str[0:67] + str[68:130]
+str = "Python is easy because it is  an interpreted, interactive,\
+        object-oriented programming language that combines\
+        remarkable power with very clear syntax\
+        also becuase of its readability, versatility\
+        and provides large and active community"
+str = str[0:57] + str[66:116] + str[75:122] + str[83:141] + str[91:156]
 print(str)
