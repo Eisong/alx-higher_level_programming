d1e28390a28773f6486398f466922be7eb76070f
Edet Isong <eddieisong1@gmail.com>
2023-12-01 09:31:47 -0800

    Python Network project by Edet Isong

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -6,18 +6,14 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-url=$1
 
-# Use curl to send a request and store the response in a variable
-response=$(curl -sI "$url")
+# Use curl to send a request and store the response
 
-# Extract the content length from the response headers
-content_length=$(echo "$response" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
+url="https://example.com/api/resource"
 
-# Check if Content-Length is present in the headers
-if [ -z "$content_length" ]; then
-    echo "Unable to retrieve Content-Length from the response headers."
-    exit 1
-fi
+# Use curl to send a GET request
+response=$(curl -s "$url")
 
-echo "Size of the response body: ${content_length} bytes"
+# Display the response
+echo "Response:"
+echo "$response"
